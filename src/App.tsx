b36a3a530ae973f1f1bd9b6a7c9c02b904c1a96d
Marco Beltempo <marco@getmaple.ca>
2025-05-01 11:26:10 -0400

    Refactor import statements in App.tsx for clarity and organization

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import "react-native-gesture-handler";
 import { DatadogProvider } from "expo-datadog";
 import { DdRumReactNavigationTracking } from "@datadog/mobile-react-navigation";
-
-import { onSDKInitialized, config } from "./Datadog";
-
 import {
   NavigationContainer,
   useNavigationContainerRef,
 } from "@react-navigation/native";
 
-import "./Datadog";
+import { onSDKInitialized, config } from "./Datadog";
 import { RootStack } from "./RootStack";
 
 export function App() {
